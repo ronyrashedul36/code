927da4224cadfee68855100de75c6bdf23c4f677
Md Rashedul Islam Rony <31292548+ronyrashedul36@users.noreply.github.com>
2018-09-05 18:44:27 +0600

    A. Game With Sticks
    Codeforces Round #258 (Div. 2)(A. Game With Sticks)

---
+++ Game With Sticks.cc
@@ -0,0 +1,27 @@
+
+#include<iostream>
+#include<bits/stdc++.h>
+#include<cstdio>
+#include<string>
+#include<cstring>
+#include<algorithm>
+#include<set>
+#include<queue>
+#include<vector>
+#include<map>
+using namespace std;
+#define si(a) scanf("%d",&a)
+#define sii(a,b) scanf("%d%d",&a,&b);
+#define siii(a,b,c) scanf("%d%d%d",&a,&b,&c)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define pi(a) printf("%d\n",a)
+#define pb push_back
+int main()
+{
+    int n,i,j,m;
+    sii(n,m);
+    i=min(n,m);
+    if(i%2) cout <<"Akshat"<<endl;
+    else cout <<"Malvica"<<endl;
+    return 0;
+}
