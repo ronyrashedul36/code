65e7cf2b624bd93136beec77e3337221f877919d
Md Rashedul Islam Rony <31292548+ronyrashedul36@users.noreply.github.com>
2019-01-11 21:37:48 +0600

    Educational Codeforces Round 58 (Rated for Div. 2)

--- a.cc
+++ a.cc
@@ -1,37 +1,25 @@
-#include<iostream>
 #include<bits/stdc++.h>
-#include<cstdio>
-#include<string>
-#include<cstring>
-#include<algorithm>
-#include<set>
-#include<queue>
-#include<vector>
-#include<map>
 using namespace std;
-#define si(a) scanf("%d",&a)
-#define sii(a,b) scanf("%d%d",&a,&b);
-#define siii(a,b,c) scanf("%d%d%d",&a,&b,&c)
-#define FOR(x,to) for(x=0;x<(to);x++)
-#define pi(a) printf("%d\n",a)
-#define pb push_back
-int main()
-{
-    long long int n,i,j,k,a,b;
-    cin>>n;
-    while(n--)
-    {
-        long long int m=0,n=0;
-        cin>>a>>b>>k;
-        if(k%2 == 0) {
-            m=k/2;
+int main(){
+    long long t,a,b,d,ans=0;
+    cin>>t;
+    while(t--){
+            ans=0;
+        cin>>a>>b>>d;
+        if(d<a || d>b){
+            cout<<d<<endl;
+           continue;
         }
-        else {
-            m=k/2+1;
+        else{
+          while(1){
+              ans=ans+d;
+                if(ans>b){
+                cout<<ans<<endl;
+                break;
+            }
+          }
         }
-       /// cout <<m <<endl;
-        cout << m*a-(k-m)*b<<endl;
-       /// pi(m*a-(k-m)*b);
     }
+
     return 0;
 }
