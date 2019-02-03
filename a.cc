12ca4967a860e67faccf08e182c1961feb8ab503
Md Rashedul Islam Rony <31292548+ronyrashedul36@users.noreply.github.com>
2019-02-03 22:56:23 +0600

    CodeCraft-19 and Codeforces Round #537 (Div. 2)

--- a.cc
+++ a.cc
@@ -1,25 +1,52 @@
+#include<iostream>
 #include<bits/stdc++.h>
+#include<cstdio>
+#include<string>
+#include<cstring>
+#include<algorithm>
+#include<set>
+#include<queue>
+#include<vector>
+#include<map>
 using namespace std;
-int main(){
-    long long t,a,b,d,ans=0;
-    cin>>t;
-    while(t--){
-            ans=0;
-        cin>>a>>b>>d;
-        if(d<a || d>b){
-            cout<<d<<endl;
-           continue;
-        }
-        else{
-          while(1){
-              ans=ans+d;
-                if(ans>b){
-                cout<<ans<<endl;
-                break;
-            }
-          }
-        }
+#define si(a) scanf("%d",&a)
+#define sii(a,b) scanf("%d%d",&a,&b)
+#define siii(a,b,c) scanf("%d%d%d",&a,&b,&c)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define pi(a) printf("%d\n",a)
+#define pb push_back
+int main()
+{
+    string s,t;
+    int i,m=0;
+    cin>>s>>t;
+    int l=s.length();
+    int l1=t.length();
+    if(l!=l1)
+    {
+        cout << "NO" << endl;
+        return 0;
     }
-
+    FOR(i,l)
+    {
+        if((s[i] == 'a' || s[i] == 'e' ||s[i] == 'i' ||s[i] == 'o' ||s[i] == 'u') && (t[i] == 'a' || t[i] == 'e' ||t[i] == 'i' ||t[i] == 'o' ||t[i] == 'u'))
+        {
+            m++;
+            continue;
+        }
+        else if(!((s[i] == 'a' || s[i] == 'e' ||s[i] == 'i' ||s[i] == 'o' ||s[i] == 'u') && !(t[i] == 'a' || t[i] == 'e' ||t[i] == 'i' ||t[i] == 'o' ||t[i] == 'u')))
+        {
+            m++;
+            continue;
+        }
+        else
+            break;
+    }
+    if(m==l)
+        cout << "Yes" << endl;
+    else
+    {
+        cout << "No" << endl;
+    }
     return 0;
 }
