ad77e6e731bd6e1876ccfe167f9b64c0b82e6de2
Md Rashedul Islam Rony <31292548+ronyrashedul36@users.noreply.github.com>
2019-04-21 12:15:25 +0600

    Forethought Future Cup - Elimination Round

--- a.cc
+++ a.cc
@@ -1,52 +1,23 @@
-#include<iostream>
 #include<bits/stdc++.h>
-#include<cstdio>
-#include<string>
-#include<cstring>
-#include<algorithm>
-#include<set>
-#include<queue>
-#include<vector>
-#include<map>
+#include<list>
+#include<iterator>
+
 using namespace std;
-#define si(a) scanf("%d",&a)
-#define sii(a,b) scanf("%d%d",&a,&b)
-#define siii(a,b,c) scanf("%d%d%d",&a,&b,&c)
-#define FOR(x,to) for(x=0;x<(to);x++)
-#define pi(a) printf("%d\n",a)
-#define pb push_back
+
 int main()
 {
-    string s,t;
-    int i,m=0;
-    cin>>s>>t;
-    int l=s.length();
-    int l1=t.length();
-    if(l!=l1)
+    int i,j,n,cnt,ans,m;
+    cnt=ans=0;
+    m=INT_MAX;
+    string s;
+    cin>>s;
+    m=s.size();
+    for(i=0;i<s.size();i++)
     {
-        cout << "NO" << endl;
-        return 0;
+        if(s[i]=='a') cnt++;
     }
-    FOR(i,l)
-    {
-        if((s[i] == 'a' || s[i] == 'e' ||s[i] == 'i' ||s[i] == 'o' ||s[i] == 'u') && (t[i] == 'a' || t[i] == 'e' ||t[i] == 'i' ||t[i] == 'o' ||t[i] == 'u'))
-        {
-            m++;
-            continue;
-        }
-        else if(!((s[i] == 'a' || s[i] == 'e' ||s[i] == 'i' ||s[i] == 'o' ||s[i] == 'u') && !(t[i] == 'a' || t[i] == 'e' ||t[i] == 'i' ||t[i] == 'o' ||t[i] == 'u')))
-        {
-            m++;
-            continue;
-        }
-        else
-            break;
-    }
-    if(m==l)
-        cout << "Yes" << endl;
-    else
-    {
-        cout << "No" << endl;
-    }
+    ans=m/2+m%2;
+    if(cnt>ans) cout << m;
+    else cout << 2*cnt-1;
     return 0;
 }
